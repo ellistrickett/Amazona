163467841ab1bf17b9b0fb4bc77a57d2c2edede6
Ellis Trickett <ellis.trickett@gmail.com>
2020-07-08 14:00:32 +0100

    set up node server

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+  products: [
+    {
+      _id:'1',
+      name: 'Bowling Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: 'OF',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id:'2',
+      name: 'Combat Trousers',
+      category: 'Trousers',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: 'OF',
+      rating: 4.0,
+      numReviews: 10
+    },
+    {
+      _id:'3',
+      name: 'Funky Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 40,
+      brand: 'OF',
+      rating: 4.2,
+      numReviews: 10
+    },
+  ]
+}
